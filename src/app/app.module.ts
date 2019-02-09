da452b4338b0308857eac5502f0013db8245882a
bend1x <hbendixlewis@gmail.com>
2019-02-09 20:58:54 +0000

    Intial work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -62,6 +62,7 @@ import { TeamComponent } from './teams/team/team.component';
   ],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- src/app/files/file/file.component.ts
+++ src/app/files/file/file.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FileService } from '../shared/file.service';
 import { File } from '../shared/file';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Types } from '../shared/types';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-file',
@@ -11,11 +12,17 @@ import { Types } from '../shared/types';
 })
 export class FileComponent implements OnInit {
 
+  @ViewChild('fileTags') fileTags: ElementRef;
+  fileForm: FormGroup;
   file: File;
   fileTypes: string[];
   sizeTypes: string[];
+  teams: [];
+  tags: string[] = [];
+  addNew = true;
 
-  constructor(private fileService: FileService) { }
+  constructor(private fileService: FileService,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
     this.file = this.fileService.getFileToView();
@@ -27,6 +34,63 @@ export class FileComponent implements OnInit {
         this.file = this.fileService.getFileToView();
       }
     });
+
+    this.fileForm = this.fb.group({
+      hideRequired: false,
+      floatLabel: 'auto',
+      version: [
+        { value: 1, disabled: true }, [
+          Validators.required
+        ]
+      ],
+      title: ['', [
+        Validators.required
+      ]],
+      description: [''],
+      fileType:  ['', [
+        Validators.required
+      ]],
+      size: [0, [
+        Validators.required,
+        Validators.min(0)
+      ]],
+      sizeType: ['', [
+        Validators.required
+      ]],
+      isPublic: ['', [
+        Validators.required
+      ]],
+    });
   }
 
+  public addTag(tag) {
+    this.fileTags.nativeElement.value = '';
+    this.tags.push(tag);
+
+  }
+
+  public removeTag (tag) {
+    this.tags.splice(tag, 1);
+    this.fileTags.nativeElement.value = '';
+  }
+
+
+  get version() {
+    return this.fileForm.get('version');
+  }
+  get title() {
+    return this.fileForm.get('title');
+  }
+  get fileType() {
+    return this.fileForm.get('fileType');
+  }
+  get size() {
+    return this.fileForm.get('size');
+  }
+  get sizeType() {
+    return this.fileForm.get('sizeType');
+  }
+  get isPublic () {
+    return this.fileForm.get('isPublic');
+  }
 }
