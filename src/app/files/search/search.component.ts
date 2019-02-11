51ac98d08ad284e434325b521d06814ed2cca017
bend1x <hbendixlewis@gmail.com>
2019-02-11 20:42:53 +0000

    search UI

--- src/app/files/search/search.component.ts
+++ src/app/files/search/search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService } from 'src/app/teams/shared/team.service';
+import { Team } from 'src/app/teams/shared/team';
+import { NotificationService } from 'src/app/shared/services/notification.service';
+import { UserService } from 'src/app/users/shared/user.service';
+import { User } from 'src/app/users/shared/user';
+import { Types } from '../shared/types';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  teams: Team[] = [];
+  users: User[] = [];
+  fileTypes: string[] = [];
+  sizeTypes: string[] = [];
+
+  constructor(private teamService: TeamService,
+    private userService: UserService,
+    private notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.teamService.getTeams()
+      .subscribe(
+        (res) => {
+          this.teams = res;
+        }, (err) => {
+          this.notificationService.triggerNotification(`Error getting teams '${ err.statusText }'`, false, 3000);
+          console.log(err);
+        }
+    );
+
+    this.userService.getAll()
+      .subscribe(
+        (res) => {
+          res.splice(res.findIndex(item => item.name ===  this.userService.getUserDetails().username), 1);
+          this.users = res;
+        }, (err) => {
+          this.notificationService.triggerNotification(`Error getting teams '${ err.statusText }'`, false, 3000);
+          console.log(err);
+        }
+      );
+
+      this.fileTypes = Types.fileTypes();
+      this.sizeTypes = Types.sizeTypes();
   }
 
 }
